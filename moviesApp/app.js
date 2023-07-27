c7a684539b3b594d871537f97fb2983d96a3eb94
kvelardi <kristenvelardi@gmail.com>
2023-07-27 09:23:58 -0400

    created outline plan for JS w/ jQuery

---
+++ moviesApp/app.js
@@ -0,0 +1,19 @@
+//  When the form is submitted, capture the values for each of the inputs and append them to the DOM along with a button to remove each title and rating from the DOM 
+//When the button to remove is clicked, remove each title and rating from the DOM
+
+// **********FUNCTIONS TO EXECUTE*********
+
+//click submit button- form data
+    //create data table
+    //submit form data to table
+    //append table to add data
+    //create delete button
+    //reset form
+
+// delete button- when clicked
+    //remove specific data from table
+    //remove from DOM
+
+//sort A-Z vice versa
+//sort low to high vice versa
+
